ff6d664ec22de0511646182f756aac16600b10e1
Rikkingg21 <x_xricardo@hotmail.com>
2025-09-15 19:50:24 -0500

    Vista de layout Corregida

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,44 +82,25 @@
             <ul class="nav nav-pills flex-column"><!--contenido-->
                 @if(session('current_role') === 'admin')
                 {{-- Solo para admin --}}
-                <li class="nav-item">
-                    <a class="nav-link text-white" data-bs-toggle="collapse" href="#collapseAdmin" role="button" aria-expanded="false" aria-controls="collapseAdmin">
-                        <i class="bi bi-person-gear me-2"></i> <span>Administración</span>
-                    </a>
-                    <div class="collapse ps-3" id="collapseAdmin">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('colegioconfig.edit') }}" class="nav-link text-white {{ request()->routeIs('colegioconfig.*') ? 'active' : '' }}">
-                                    <i class="bi bi-building me-2"></i> <span>Colegio</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="" class="nav-link text-white {{ request()->routeIs('users.*') ? 'active' : '' }}">
-                                    <i class="bi bi-people me-2"></i> <span>Usuarios</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('maya.index') }}" class="nav-link text-white {{ request()->routeIs('maya.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Mayas
-                    </a>
-                </li>
 
                 <li class="nav-item">
-                    <a href="{{ route('colegioconfig.edit') }}" class="nav-link text-white {{ request()->routeIs('colegioconfig.*') ? 'active' : '' }}">
-                        <i class="bi bi-building me-2"></i> Colegio
+                    <a href="{{ route('maya.index') }}" class="nav-link text-white {{ request()->routeIs('maya.*') ? 'active' : '' }}">
+                        <i class="bi bi-clipboard2-check me-2"></i> <span>Mayas</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="" class="nav-link text-white {{ request()->routeIs('users.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Usuarios
+                    <a href="{{ route('colegioconfig.edit') }}" class="nav-link text-white {{ request()->routeIs('colegioconfig.*') ? 'active' : '' }}">
+                        <i class="bi bi-building me-2"></i> <span>Colegio</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('user.index') }}" class="nav-link text-white {{ request()->routeIs('users.*') ? 'active' : '' }}">
+                        <i class="bi bi-people me-2"></i> <span>Usuarios</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('reporte.index') }}" class="nav-link text-white {{ request()->routeIs('reporte.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Reportes
+                        <i class="bi bi-megaphone me-2"></i> <span>Reportes</span>
                     </a>
                 </li>
 
@@ -127,70 +108,63 @@
 
                 @if(session('current_role') === 'director')
                 <li class="nav-item">
-                    <a href="" class="nav-link text-white {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
-                        <i class="bi bi-speedometer2 me-2"></i> Dashboard
+                    <a href="{{ route('director.dashboard') }}" class="nav-link text-white {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                        <i class="bi bi-speedometer2 me-2"></i> <span>Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('user.index') }}" class="nav-link text-white {{ request()->routeIs('users.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Usuarios
+                        <i class="bi bi-people me-2"></i> <span>Usuarios</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('grado.index') }}" class="nav-link text-white {{ request()->routeIs('grado.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Grados
+                        <i class="bi bi-person-rolodex me-2"></i> <span>Grados</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('materia.index') }}" class="nav-link text-white {{ request()->routeIs('materia.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Materias
+                        <i class="bi bi-journal-text me-2"></i> <span>Materias</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('conducta.index') }}" class="nav-link text-white {{ request()->routeIs('conducta.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Conducta
+                        <i class="bi bi-people me-2"></i> <span>Conducta</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('maya.index') }}" class="nav-link text-white {{ request()->routeIs('maya.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Mayas
+                         <i class="bi bi-clipboard2-check me-2"></i> <span>Mayas</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('reporte.index') }}" class="nav-link text-white {{ request()->routeIs('reporte.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Reportes
+                        <i class="bi bi-megaphone me-2"></i> <span>Reportes</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('asistencia.index') }}" class="nav-link text-white {{ request()->routeIs('asistencia.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Asistencia
+                        <i class="bi bi-journal-check me-2"></i> <span>Asistencia</span>
                     </a>
                 </li>
-
-
                 @endif
 
                 @if(session('current_role') === 'docente')
-                {{-- Ejemplo para docente --}}
 
                 <li class="nav-item">
-                    <a href="{{ route('maya.index') }}" class="nav-link text-white {{ request()->routeIs('maya.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Mayas
+                    <a href="{{ route('docente.dashboard') }}" class="nav-link text-white {{ request()->routeIs('docente.*') ? 'active' : '' }}">
+                        <i class="bi bi-speedometer2 me-2"></i> Dashboard
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('docente.dashboard') }}" class="nav-link text-white">
-                        <i class="bi bi-journal-text me-2"></i> Mis Cursos
+                    <a href="{{ route('maya.index') }}" class="nav-link text-white {{ request()->routeIs('maya.*') ? 'active' : '' }}">
+                         <i class="bi bi-clipboard2-check me-2"></i> <span>Mayas</span>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="{{ route('reporte.index') }}" class="nav-link text-white {{ request()->routeIs('reporte.*') ? 'active' : '' }}">
                         <i class="bi bi-people me-2"></i> Reportes
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('asistencia.index') }}" class="nav-link text-white {{ request()->routeIs('asistencia.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Asistencia
                     </a>
                 </li>
                 @endif
@@ -198,19 +172,19 @@
                 @if(session('current_role') === 'auxiliar')
                 <li class="nav-item">
                     <a href="{{ route('reporte.index') }}" class="nav-link text-white {{ request()->routeIs('reporte.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Reportes
+                        <i class="bi bi-megaphone me-2"></i> <span>Reportes</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('asistencia.index') }}" class="nav-link text-white {{ request()->routeIs('asistencia.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Asistencia
+                        <i class="bi bi-journal-check me-2"></i> <span>Asistencia</span>
                     </a>
                 </li>
                 @endif
                 @if(session('current_role') === 'apoderado')
                 <li class="nav-item">
                     <a href="{{ route('reporte.index') }}" class="nav-link text-white {{ request()->routeIs('reporte.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Reportes
+                        <i class="bi bi-megaphone me-2"></i> <span>Reportes</span>
                     </a>
                 </li>
                 @endif
@@ -218,7 +192,7 @@
                 <li class="nav-item">
                     <a href="{{ route('libreta.index', ['anio' => date('Y'), 'bimestre' => 1]) }}"
                     class="nav-link text-white {{ request()->routeIs('libreta.*') ? 'active' : '' }}">
-                        <i class="bi bi-people me-2"></i> Libreta
+                        <i class="bi bi-journal-bookmark me-2"></i> <span>Libreta</span>
                     </a>
                 </li>
                 @endif
