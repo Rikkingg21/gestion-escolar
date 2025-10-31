d6ae83082a6c8df4fa6171191b731d92b39b090e
Rikkingg21 <x_xricardo@hotmail.com>
2025-10-31 11:35:42 -0500

    Restricciones para modulo Conducta segun rol

--- app/Http/Controllers/ConductaController.php
+++ app/Http/Controllers/ConductaController.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\DB;
 
 class ConductaController extends Controller
 {
+    //moduleID 12 = Conducta
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $user = auth()->user();
-            if (!$user->hasRole('admin') && !$user->hasRole('director') && !$user->hasRole('docente')) {
-                abort(403, 'Acceso no autorizado.');
+            if (!auth()->user()->canAccessModule('12')) {
+                abort(403, 'No tienes permiso para acceder a este módulo.');
             }
             return $next($request);
         });
