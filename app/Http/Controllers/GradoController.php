e2b6624ca7af13f91060e087155f8a739086b2a7
Rikkingg21 <x_xricardo@hotmail.com>
2025-10-31 11:30:08 -0500

    Restricciones para modulo Grados segun rol

--- app/Http/Controllers/GradoController.php
+++ app/Http/Controllers/GradoController.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 
 class GradoController extends Controller
 {
+    //moduleID 10 = Grados
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $user = auth()->user();
-            if (!$user->hasRole('admin') && !$user->hasRole('director') && !$user->hasRole('docente')) {
-                abort(403, 'Acceso no autorizado.');
+            if (!auth()->user()->canAccessModule('10')) {
+                abort(403, 'No tienes permiso para acceder a este módulo.');
             }
             return $next($request);
         });
