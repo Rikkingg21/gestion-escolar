cdfc89fe61c0529842d8bb2f8a6b4e457d1ef4ae
Rikkingg21 <x_xricardo@hotmail.com>
2025-10-31 09:24:20 -0500

    Restricciones para MateriaCriterio segun rol y modulo

--- app/Http/Controllers/Materia/MateriaCriterioController.php
+++ app/Http/Controllers/Materia/MateriaCriterioController.php
@@ -14,13 +14,13 @@ class MateriaCriterioController extends Controller
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $user = auth()->user();
-            if (!$user->hasRole('admin') && !$user->hasRole('director') && !$user->hasRole('docente')) {
-                abort(403, 'Acceso no autorizado.');
+            if (!auth()->user()->canAccessModule('Materias')) {
+                abort(403, 'No tienes permiso para acceder a este módulo.');
             }
             return $next($request);
         });
     }
+
 
     public function index($id, Request $request)
     {
